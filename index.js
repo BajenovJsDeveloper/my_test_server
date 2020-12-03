9ca411bef46ddfb7a4d4a1f7c40138c6a0c39ba5
bajenovjsdeveloper <bajenovcpm433@yandex.ru>
2020-12-03 22:49:55 +0200

    add routes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const messages = [];
 
-app.get('/hello',(req, res) => {
+app.use(express.json());
 
-	res.send(req.body)
+app.post('/hello',(req, res) => {
+	messages.push({data: req.body})
+	res.sendStatus(200);
+})
+app.get('/hello/1',(req, res)=>{
+
+	res.json({status: 'OK', payload: messages});
 })
 
 app.listen(PORT, () =>{
-	console.log('Server running...');
+	console.log('Server running...on port', PORT);
 })

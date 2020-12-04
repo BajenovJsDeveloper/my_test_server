e1055c31db86fabb95813f631fb1ce53a826e0db
bajenovjsdeveloper <bajenovcpm433@yandex.ru>
2020-12-04 18:17:29 +0200

    add send message method

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const msgaxios = require('./lib/msgaxios.js');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const messages = [];
+const TOKEN = 'xoxb-120579660452-1572251223328-22PjsNaWYSTCiuk7aL4v12Z7';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })) 
 
 app.post('/hello',(req, res) => {
 	messages.push({data: req.body, params: req.params})
-	console.log('Params: ',req.params)
+	const dataObj = {
+		channelID: req.body.channel_id,
+		token: TOKEN,
+		text: req.body.text
+	}
+	msgaxios(dataObj);
 	res.sendStatus(200);
 })
 app.get('/hello/info',(req, res)=>{
-
 	res.json({status: 'OK', payload: messages});
 })
 

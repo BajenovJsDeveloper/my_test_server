5de0b6b43e36af1a16f35349dc4e44c82a38b7c0
bajenovjsdeveloper <bajenovcpm433@yandex.ru>
2020-12-05 01:13:27 +0200

    add Channel message hook

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const pidCrypt = require("pidcrypt");
 const express = require('express');
 const msgaxios = require('./lib/msgaxios.js');
+const msghook = require('./lib/msghook.js');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const crypted = 'U2FsdGVkX18miheDr1a4vy5RuOAoHZsA197H6IgexVNZwZJa8Z3FF5T0Y2bon/Tb9U0Yjuke4SFEN9C6A4g9LvL/Zh7Ux4b0b/GsezOB1dk=';
+const cryptedToken = "U2FsdGVkX18miheDr1a4vy5RuOAoHZsA197H6IgexVNZwZJa8Z3FF5T0Y2bon/Tb9U0Yjuke4SFEN9C6A4g9LvL/Zh7Ux4b0b/GsezOB1dk=";
 require("pidcrypt/aes_cbc");
 const pw =  "hello world";
+const tokenHook = "U2FsdGVkX18e7tkHOyqtkVl1Hl+GKykk8dkSLCA+oJ8Uf9M8Ocoj6QAgrBBBlvMm6y1J9PPP+9898xc5SCy7lg==";
+const URLHookMessage = "https://hooks.slack.com/services/";
+const URLPostMeaasge = "https://slack.com/api/chat.postMessage";
 const aes = new pidCrypt.AES.CBC();
 const messages = [];
 
-const decrypted = aes.decryptText(crypted, pw);
+const decryptedToken = aes.decryptText(cryptedToken, pw);
+const decryptedHookToken = aes.decryptText(tokenHook, pw);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })) 
 
-app.post('/hello',(req, res) => {
-	messages.push({data: req.body, params: req.params})
-	const dataObj = {
-		channelID: req.body.channel_id,
-		token: decrypted,
-		text: req.body.text
-	}
-	msgaxios(dataObj).then(resolve => {
-		res.sendStatus(200);	
+app.post("/hello",(req, res) => {
+	messages.push({
+		data: req.body,
+		params: req.params,
+		date: new Date().toLocaleDateString()
 	});
+	const dataObj = {}
+	const messsage = req.body.text;
+	const channel = req.body.channel_id;
+	
+	if(messsage.search("#general ") != -1){
+		dataObj.text = messsage.replace(/#general/gi,"");
+		dataObj.token = decryptedHookToken;
+		dataObj.url = URLHookMessage;
+		console.log('DATA: ', dataObj)
+		msghook(dataObj).then(resolve => {
+			res.sendStatus(200);	
+		});
+	}
+	else{
+		dataObj.channelID = channel;
+		dataObj.token = decryptedToken;
+		dataObj.text = messsage;
+		dataObj.url = URLPostMeaasge;
+		msgaxios(dataObj).then(resolve => {
+			res.sendStatus(200);	
+		});
+	}
 })
 app.get('/hello/info',(req, res)=>{
 	res.json({status: 'OK', payload: messages});
